2f71fb216db6f8c9ea6d51a5416e2583e598705d
Mishu-21 <165667286+Mishu-21@users.noreply.github.com>
2024-04-04 17:22:23 +0530

    Add files via upload

---
+++ Continue_Keyword.java
@@ -0,0 +1,16 @@
+package grotechjava;
+
+public class Continue_Keyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=0;i<=5;i++)
+		{
+			if(i==4)
+				continue;
+			System.out.println("My age is "+i);
+		}
+	}
+
+}
