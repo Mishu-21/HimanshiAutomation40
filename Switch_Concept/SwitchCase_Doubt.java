1c8f5b820ea9ac0d00967aed0b8d47a77937654f
Himanshi <chauhanhimanshi051@gmail.com>
2024-04-17 15:51:18 +0530

    Switch Concept

---
+++ Switch_Concept/SwitchCase_Doubt.java
@@ -0,0 +1,25 @@
+package grotechjava;
+
+import java.util.Scanner;
+
+public class SwitchCase_Doubt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//agr char input lena hua toh aise nhi kar skte ya toh phle he case aposs krdo bcz human imput cant take 
+		/*Scanner s=new Scanner(System.in);
+		char s1=s.next();*/
+		switch('A')
+		{
+		case 'A': 
+			System.out.println("Logic 1");
+			break;
+		case 'B': 
+			System.out.println("Logic 2");
+			break;
+	}
+	}
+}
+
+
---
+++ Switch_Concept/Switch_Class.java
@@ -0,0 +1,28 @@
+package grotechjava;
+
+public class Switch_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		switch(2)
+		{
+		case 1: 
+			System.out.println("Logic 1");
+			break;
+		case 2: 
+			System.out.println("Logic 2");
+			break;
+		case 3: 
+			System.out.println("Logic 3");
+			break;
+		case 4: 
+			System.out.println("Logic 4");
+			break;
+		default:
+			System.out.println("sorryy....");
+		}
+
+	}
+
+}
