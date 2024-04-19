1adb6eab576e1811031c3f4cd875c901ca54b776
Himanshi <chauhanhimanshi051@gmail.com>
2024-04-19 23:52:15 +0530

    While Loop

---
+++ WhileLoop_1.java
@@ -0,0 +1,17 @@
+package grotechjava;
+
+public class WhileLoop_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a=10;
+		while(a<12)
+		{
+			System.out.println("hello");
+			a++;		
+		}
+	}
+
+}
